aae1c1931ffae8b453c582e165fa0c87a1d7fadf
asmapa <asmapa122002@gmail.com>
2025-06-14 10:40:25 +0530

    updated db

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,31 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+//must include .js at the end and also export it from that particular file
+import productRoutes from "./routers/productRouters.js"
+
+//working of environmental variable 
+
+import dotenv from "dotenv"
+
+dotenv.config();
+const PORT = process.env.PORT;
+console.log(PORT)
+
+
 
 const app = express();
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
 
-app.get("/test", (req, res) => {
-    res.send("Hello server is running");
-})
 
-app.listen(3000, () => {
-    console.log("Server is running in port 3000");
+//for made everything simple we create router and controller folders so we didnt specify API here ,
+
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () => {
+    console.log("Server is running in port "+PORT);
 })
