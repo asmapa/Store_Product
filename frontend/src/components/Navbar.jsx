606693905d3d0f0370c337fc50968ab0ad480d7d
asmapa <asmapa122002@gmail.com>
2025-06-20 18:27:41 +0530

    updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
-
+import { useState } from 'react'
+import asma from "../assets/asmapa.png"
 const Navbar = () => {
+  const [DropDown, setDropDown] = useState(false);
+
+  const ToggleDropDown = () => {
+    setDropDown(!DropDown);
+  }
   return (
-      <div className="navbar bg-base-100 shadow-sm">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
+    <div>
+      <nav className=' p-3 shadow-lg'>
+        <div className='container mx-auto flex space-x-6 '>
+          <div>
+            <img src={ asma} className='w-20 h-10 '/>
+          </div>
+          <div className='text-xl font-playfair text-purple-900 font-extrabold '> 
+            Queenly Cart
+          </div>
+          <ul className='flex space-x-5 text-pink-700 font-extrabold'>
+            
+           
+            <li><button onClick={() => onCategoryChange("All")}>Ornaments</button></li>
+            <li><button onClick={() => onCategoryChange("Dresses")}>Dresses</button></li>
+            <li><button onClick={() => onCategoryChange("Ornaments")}>Beauty Products</button></li>
+            <li><button onClick={() => onCategoryChange("Pads")}>Pads</button></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/cart">🛒 Cart</a></li>
+          </ul>
+        </div>
+        
+
+       
+      </nav>
+    </div>
   )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        playfair: ['Playfair Display', 'serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+        dancing: ['Dancing Script', 'cursive'],
+      },
+},
+    
   },
   plugins: [daisyui],
 }
